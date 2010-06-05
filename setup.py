f55d8fa3738651ccdc98b1473fc74c8bb3ea385b
Friedrich Weber <fred@reichbier.de>
2010-06-06 01:34:24 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = 'Application Indicators Contrib Libraries',
+    version = '0.1',
+    package_dir = {'cream.contrib.appindicators': 'appindicators'},
+    packages = ['cream.contrib.appindicators'],
+    )
